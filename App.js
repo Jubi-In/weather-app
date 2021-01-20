63255a1d28bc52e85ac3d6138798e8633578bf85
jubi-in <thetruthrunswild@naver.com>
2021-01-20 17:17:10 +0900

    2021-01-20

--- App.js
+++ App.js
@@ -1,6 +1,55 @@
 import React from 'react';
+import { Alert } from "react-native";
 import Loading from "./Loading";
+import * as Location from "expo-location";
+import axios from "axios";
+import Weather from "./Weather"
 
-export default function App() {
-  return <Loading />;
+const API_KEY = "c7fc6f6ed5b0e23dd46a4e12a433bf2f";
+
+export default class extends React.Component {
+  state = {
+    isLoading: true
+  };
+
+  getWeather = async(latitude, longitude) => {
+    const { 
+      data: {
+        main: { temp },
+        weather
+    } 
+  } = await axios.get(
+      `http://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=${API_KEY}&units=metric`
+      );
+
+    this.setState({ 
+      isLoading: false, 
+      condition: weather[0].main,
+      temp
+    });
+  };
+  
+  getLocation = async() => {
+    try {
+      await Location.requestPermissionsAsync();
+      const { coords: { latitude, longitude} } 
+      = await Location.getCurrentPositionAsync();
+      this.getWeather(latitude, longitude);
+      this.setState({ isLoading: false });
+    } catch (error) {
+      Alert.alert("Can't find you.", "So sad");
+    }
+  };
+  componentDidMount() {
+    this.getLocation();
+  }
+
+  render() {
+    const { isLoading, temp, condition } = this.state;
+    return isLoading ? (
+      <Loading />
+    ) : (
+      <Weather temp={Math.round(temp)} condition={condition} />
+    );
+  }
 }
